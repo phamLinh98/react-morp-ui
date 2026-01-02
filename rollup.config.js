1f20694176331afb32f5a204d4273e842d3391f4
phamLinh98 <linhthusinh98@gmail.com>
2026-01-03 00:58:43 +0900

    feat: update package configuration for Rollup integration and add initial Rollup config file

---
+++ rollup.config.js
@@ -0,0 +1,28 @@
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import babel from '@rollup/plugin-babel';
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+
+export default {
+    input: 'src/index.js',
+    output: [
+        {
+            file: 'dist/index.cjs.js',
+            format: 'cjs',
+            exports: 'named',
+        },
+        {
+            file: 'dist/index.esm.js',
+            format: 'esm',
+        },
+    ],
+    plugins: [
+        peerDepsExternal(),
+        resolve(),
+        commonjs(),
+        babel({
+            babelHelpers: 'bundled',
+            presets: ['@babel/preset-react'],
+        }),
+    ],
+};
