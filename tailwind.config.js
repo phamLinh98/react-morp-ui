1b127a4a32e9c7b6d861316678fda30c5ed1df8c
phamLinh98 <linhthusinh98@gmail.com>
2026-01-03 02:01:16 +0900

    fix: Button export

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,10 @@
-/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
 export default {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,jsx}",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
